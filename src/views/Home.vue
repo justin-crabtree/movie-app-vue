64b74281eeb1c29db291d688f3cd21f51e4a6844
crabtree <justin.kyle.crabtree@gmail.com>
2019-10-07 19:36:14 -0400

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,76 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>New Movie</h1>
+    <div>
+      Title:
+      <input type="text" v-model="newMovieTitle" />
+      Year:
+      <input type="text" v-model="newMovieYear" />
+      Plot:
+      <input type="text" v-model="newMoviePlot" />
+      Director:
+      <input type="text" v-model="newMovieDirector" />
+    </div>
+    <h1>All Movies</h1>
+    <div v-for="movie in movies">
+      <h2>{{ movie.title }}</h2>
+      <button v-on:click="showMovieInfo(movie)">More Info</button>
+      <div v-if="currentMovie === movie">
+        Year:
+        <h2>{{ movie.year }}</h2>
+        Plot:
+        <h2>{{ movie.plot }}</h2>
+        Director:
+        <h2>{{ movie.director }}</h2>
+      </div>
+    </div>
   </div>
 </template>
 
+<style></style>
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      movies: [],
+      currentMovie: {},
+      newMovieTitle: "",
+      newMovieYear: "",
+      newMoviePlot: "",
+      newMovieDirector: ""
+    };
+  },
+  created: function() {
+    axios.get("/api/movies").then(response => {
+      this.movies = response.data;
+    });
+  },
+  methods: {
+    createMovie: function() {
+      var params = {
+        title: this.newMovieTitle,
+        year: this.newMovieYear,
+        plot: this.newMoviePlot,
+        director: this.newMovieDirector
+      };
+      axios.post("/api/movies", params).then(response => {
+        this.movies.push(response.data);
+        this.newMovieTitle = "";
+        this.newMovieYear = "";
+        this.newMoviePlot = "";
+        this.newMovieDirector = "";
+      });
+    },
+    showMovieInfo: function(movie) {
+      if (this.currentMovie === movie) {
+        this.currentMovie = {};
+      } else {
+        this.currentMovie = movie;
+      }
+    }
   }
-}
+};
 </script>
