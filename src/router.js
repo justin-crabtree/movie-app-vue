38f34f03e8729cef1dae4dce9e1330f8616fa17f
crabtree <justin.kyle.crabtree@gmail.com>
2019-10-16 18:43:40 -0400

    adds authentication signup/login/logout

--- src/router.js
+++ src/router.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Signup fom "./views/Signup.vue";
+import Login from "./views/Login.vue";
+import Logout from "./views/Logout.vue";
+
 
 Vue.use(Router)
 
@@ -20,6 +24,21 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    }, 
+    { 
+      path: "/signup", 
+      name: "signup", 
+      component: Signup 
+    }, 
+    { 
+      path: "/login", 
+      name: "login", 
+      component: Login 
+    }, 
+    { 
+      path: "/logout", 
+      name: "logout", 
+      component: Logout 
     }
   ]
 })
